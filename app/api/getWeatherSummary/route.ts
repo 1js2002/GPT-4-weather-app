a4740d3434e155e8ef9e8804963ceedb87b264fe
1js2002 <dgoud99120@gmail.com>
2023-05-07 18:36:49 +0530

    merging everything

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -2,10 +2,7 @@ import { NextResponse } from "next/server";
 import openai from "@/openai";
 
 export async function POST(request: Request) {
-  // Weatherdata in the body of the POST request
   const { weatherData } = await request.json();
-
-  console.log("WEATHER DATA IS: ", weatherData);
 
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
@@ -15,20 +12,25 @@ export async function POST(request: Request) {
     messages: [
       {
         role: "system",
-        content: `Pretend you're a weather news presenter, presenting live of television. Introduce yourself as Willow the AI weather assitant. Be energetic and enthusiastic. Then give a summary of the weather for the next 24 hours. Make it easy for the viewer to understand and know what to do to prepare for the weather conditions, such as where sunscreen if the UV is high. Use the uv_index data to provide UV advice. Assume the data came from your team at the new office and not the user. Refer to the user as "you". Make sure your summary is no longer than 20 seconds.`,
+        content: `Pretend you're a weather news presenter presenting LIVE on television. Be energetuc and full of charisma.
+            Introduce yourself as Thiagones and say you are LIVE from Brazil. 
+            tate the city you are providing a summary for. Then give a summary of today's weather only.
+            Make it easy for the viewers to understand know what to do to prepare for those weather conditions such as wear SPF if the UV is high, etc.
+            Us the uv_index data provided to provide UV advice. Provide a joke regarding the weather.
+            Assume the data came from your team at the news office and not the user.`,
       },
       {
         role: "user",
-        content: `Hi there, can I get a summary of todays weather, use the following information to get the weather data ${JSON.stringify(
-          weatherData
-        )}`,
+        content: `Hi there, can I get a summary of today's weather, use the following information to get weather data:
+            ${JSON.stringify(weatherData)}`,
       },
     ],
   });
 
-  const { data } = response;
+  const {data} = response;
 
-  console.log("DATA IS: ", data);
+  console.log('Data: ', data);
 
-  return NextResponse.json(data.choices[0].message);
+  return NextResponse.json(data.choices[0].message)
+  
 }
--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -5,11 +5,11 @@ import InformationPanel from "@/components/InformationPanel";
 import RainChart from "@/components/RainChart";
 import StatCard from "@/components/StatCard";
 import TempChart from "@/components/TempChart";
-import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+import fecthWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
 import cleanData from "@/lib/cleanData";
 import getBasePath from "@/lib/getBasePath";
 
-export const revalidate = 43200;
+export const revalidate = 60;
 
 type Props = {
   params: {
@@ -23,18 +23,21 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
   const client = getClient();
 
   const { data } = await client.query({
-    query: fetchWeatherQuery,
+    query: fecthWeatherQuery,
     variables: {
       current_weather: "true",
-      longitude: long,
       latitude: lat,
+      longitude: long,
       timezone: "GMT",
     },
   });
 
   const results: Root = data.myQuery;
 
+  console.log("Results: ", results);
+
   const dataToSend = cleanData(results, city);
+  console.log("dataToSend: ", dataToSend);
 
   const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
     method: "POST",
@@ -46,50 +49,52 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
     }),
   });
 
-  // const GPTdata = await res.json();
-  // const { content } = GPTdata;
+  const GPTData = await res.json();
+
+  const { content } = GPTData;
 
   return (
-    <div className="flex flex-col min-h-screen md:flex-row bg-[#fafafa]">
-      <InformationPanel city={city} long={long} lat={lat} results={results} />
+    <div className="flex flex-col min-h-screen md:flex-row">
+      {/* Information Pannel */}
+      <InformationPanel city={city} lat={lat} long={long} results={results} />
 
       <div className="flex-1 p-5 lg:p-10">
         <div className="p-5">
           <div className="pb-5">
-            <h2 className="text-xl font-bold">Todays Overview</h2>
+            <h2 className="text-xl font-bold">Today Overview</h2>
             <p className="text-sm text-gray-400">
-              Last Updated at: {""}
+              Last Updated at:{" "}
               {new Date(results.current_weather.time).toLocaleString()} (
-              {results.timezone} )
+              {results.timezone})
             </p>
           </div>
 
           <div className="m-2 mb-10">
-            <CalloutCard message="sdj" />
+            <CalloutCard message="chatgpt response goes here"/>
           </div>
 
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
             <StatCard
               title="Maximum Temperature"
-              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}˚C`}
-              color="blue"
+              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
+              color="yellow"
             />
 
             <StatCard
               title="Minimum Temperature"
-              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}˚C`}
-              color="blue"
+              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
+              color="green"
             />
 
             <div>
               <StatCard
                 title="UV Index"
                 metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
-                color="blue"
+                color="rose"
               />
               {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
                 <CalloutCard
-                  message={"The UV is high today, be sure to wear sunscreen"}
+                  message="The UV is high today, be sure to wear SPF!"
                   warning
                 />
               )}
@@ -98,14 +103,14 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
             <div className="flex space-x-3">
               <StatCard
                 title="Wind Speed"
-                metric={`${results.current_weather.windspeed.toFixed(1)} km/s`}
-                color="sky"
+                metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+                color="cyan"
               />
 
               <StatCard
                 title="Wind Direction"
-                metric={`${results.current_weather.winddirection.toFixed(1)}˚`}
-                color="sky"
+                metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+                color="violet"
               />
             </div>
           </div>
